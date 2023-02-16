cd89fea100a5dcd8f7d37e9422e4ce47374145ad
daffaputra09 <daffaputra863@gmail.com>
2023-02-16 09:54:38 +0700

    baru

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Poppins": ["Poppins", "Sans-serif"]
+      },
+      colors: {
+        'primary': '#EEBE21',
+        'darkprimary': '#BE8F15',
+        'lightprimary': '#F2D642',
+        'second' : '#313131',
+        'base': '#181818',
+      },
+    },
+  },
+  plugins: [],
+}
