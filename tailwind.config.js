8ebcd7e1c9c353784320eae8b89b60bb64ddde69
daffaputra09 <daffaputra863@gmail.com>
2023-03-08 14:40:31 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'primary': '#EEBE21',
         'darkprimary': ' #d5a710',
         'lightprimary': '#f4d671',
-        'second' : '#313131',
+        'second' : '#2C2B3A',
         'base': '#0f0e17',
       },
     },
